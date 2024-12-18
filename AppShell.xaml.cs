de8173acc0ee2171b457a63f462a3f4dd7680971
Swayam Maharjan <swayam.maharjan.@gmail.com>
2024-12-18 11:15:46 +0545

    Initial commit - Empty MAUI project

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Courseworkdotnetmaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Courseworkdotnetmaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
